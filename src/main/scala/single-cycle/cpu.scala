b4e2cb1003135f4f389e029a0f06f543f6ee59cc
Hoa Nguyen <hoanguyen@ucdavis.edu>
2023-01-22 22:49:17 -0800

    Add the solution
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- src/main/scala/single-cycle/cpu.scala
+++ src/main/scala/single-cycle/cpu.scala
@@ -23,12 +23,8 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   val controlTransfer = Module(new ControlTransferUnit())
   val (cycleCount, _) = Counter(true.B, 1 << 30)
 
-  control.io := DontCare
-  registers.io := DontCare
-  aluControl.io := DontCare
-  alu.io := DontCare
-  immGen.io := DontCare
-  controlTransfer.io := DontCare
+  immGen.io <> DontCare
+  controlTransfer.io <> DontCare
   io.dmem <> DontCare
 
   //FETCH
@@ -41,8 +37,24 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   } .otherwise {
     instruction := io.imem.instruction(31, 0)
   }
-  
-  // Your code goes here
+
+  control.io.opcode := instruction(6, 0)
+
+  registers.io.readreg1 := instruction(19, 15)
+  registers.io.readreg2 := instruction(24, 20)
+  registers.io.writereg := instruction(11, 7)
+  registers.io.writedata := alu.io.result
+  registers.io.wen := (registers.io.writereg =/= 0.U) & (control.io.writeback_valid === 1.U)
+
+  aluControl.io.aluop  := control.io.aluop
+  aluControl.io.funct7 := instruction(31, 25)
+  aluControl.io.funct3 := instruction(14, 12)
+
+  alu.io.operation := aluControl.io.operation
+  alu.io.operand1 := registers.io.readdata1
+  alu.io.operand2 := registers.io.readdata2
+
+  pc := pc + 4.U
 }
 
 /*
